8adeeda338e0f5973c909943af82472bc1903bd6
cristian llanos <cristian.llanos@gmail.com>
2020-07-11 02:54:55 -0400

    FIX: Mesa entity

--- src/main/java/cl/crllanos/culinary/entity/MesaEntity.java
+++ src/main/java/cl/crllanos/culinary/entity/MesaEntity.java
@@ -14,4 +14,28 @@ public class MesaEntity {
     @Column(name="NOMBRE")
     private String nombre;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("MesaEntity{");
+        sb.append("id=").append(id);
+        sb.append(", nombre='").append(nombre).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
